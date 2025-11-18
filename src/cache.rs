2260b1e064f22561578eb4b38fed54413fdcffcf
hlsxx <holespato@gmail.com>
2025-11-18 20:08:12 +0100

    feat: GeoCacheConfig + builder

--- src/cache.rs
+++ src/cache.rs
@@ -1,4 +1,9 @@
-use std::{collections::HashMap, error::Error};
+use std::{
+  collections::HashMap,
+  error::Error,
+  fs::File,
+  path::{Path, PathBuf},
+};
 
 use crate::{
   errors::CountryCodeError, geo::convert_coords_into_microdeg, throw_country_code_error,
@@ -52,6 +57,62 @@ impl CacheKey {
   }
 }
 
+#[derive(Default)]
+struct GeoCacheConfigBuilder {
+  file_path: Option<PathBuf>,
+  memory_max_size: Option<usize>,
+  disk_max_size: Option<usize>,
+}
+
+impl GeoCacheConfigBuilder {
+  pub fn file_path(mut self, file_path: impl Into<PathBuf>) -> Self {
+    self.file_path = Some(file_path.into());
+    self
+  }
+
+  pub fn memory_max_size(mut self, size: usize) -> Self {
+    self.memory_max_size = Some(size);
+    self
+  }
+
+  pub fn disk_max_size(mut self, size: usize) -> Self {
+    self.disk_max_size = Some(size);
+    self
+  }
+
+  pub fn build(self) -> GeoCacheConfig {
+    let default = GeoCacheConfig::default();
+
+    GeoCacheConfig {
+      file_path: self.file_path.unwrap_or(default.file_path),
+      memory_max_size: self.memory_max_size.unwrap_or(default.memory_max_size),
+      disk_max_size: self.disk_max_size.unwrap_or(default.disk_max_size),
+    }
+  }
+}
+
+struct GeoCacheConfig {
+  file_path: PathBuf,
+  memory_max_size: usize,
+  disk_max_size: usize,
+}
+
+impl Default for GeoCacheConfig {
+  fn default() -> Self {
+    Self {
+      file_path: PathBuf::from("./geocache.db"),
+      memory_max_size: 100 * 1024 * 1024, // 100MB,
+      disk_max_size: 1024 * 1024 * 1024,  // 1GB,
+    }
+  }
+}
+
+impl GeoCacheConfig {
+  pub fn builder() -> GeoCacheConfigBuilder {
+    GeoCacheConfigBuilder::default()
+  }
+}
+
 pub struct GeoCache {
   data: HashMap<CacheKey, Address>,
 }
@@ -61,6 +122,10 @@ impl GeoCache {
     Self {
       data: HashMap::new(),
     }
+  }
+
+  pub fn init(&mut self) {
+    // let file = File::open(path)
   }
 
   /// Inserts a new key into `GeoCache` data.
@@ -89,10 +154,28 @@ impl GeoCache {
 
 #[cfg(test)]
 mod tests {
+  use std::path::PathBuf;
+
   use crate::{
-    cache::{CacheKey, GeoCache},
+    cache::{CacheKey, GeoCache, GeoCacheConfig},
     errors::{CountryCodeError, GeoCoordError},
   };
+
+  #[test]
+  fn geocache_config_builder() {
+    let geocache_builder = GeoCacheConfig::builder()
+      .file_path("./custom_path.db")
+      .memory_max_size(555)
+      .disk_max_size(333)
+      .build();
+
+    assert_eq!(geocache_builder.memory_max_size, 555);
+    assert_eq!(geocache_builder.disk_max_size, 333);
+    assert_eq!(
+      geocache_builder.file_path,
+      PathBuf::from("./custom_path.db")
+    );
+  }
 
   #[test]
   fn test_cache_key_try_new() {
